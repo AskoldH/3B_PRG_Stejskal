c57960b0d085a2be9d3331bdeb55788800484b15
Askold Horčička <askold.horcicka@olc.cz>
2021-11-19 09:21:20 +0100

    19. 11. 2021

--- Pleski's notes/009_while.py
+++ Pleski's notes/009_while.py
@@ -1,46 +1,88 @@
 # # 1 - heslo
-# input_password = input("Zadejte heslo: ")
-# password = "monitor"
-# attempt = 1
-#
-# while True:
-#     if input_password == password:
-#         print(f"""Heslo zadáno správně a to na {attempt}. pokus!""")
-#         break
-#
-#     attempt += 1
-#     input_password = input("Heslo bylo zadáno špatně, zadejte ho znovu: ")
+def heslo():
+    input_password = input("Zadejte heslo: ")
+    password = "monitor"
+    attempt = 1
+
+    while True:
+        if input_password == password:
+            print(f"""Heslo zadáno správně a to na {attempt}. pokus!""")
+            break
+
+        attempt += 1
+        input_password = input("Heslo bylo zadáno špatně, zadejte ho znovu: ")
 
 
 # # 2 - součet n hodnot
-# n = int(input("Zadejte číslo n: "))
-# i = 1
-# total = 0
-#
-# while i <= n:
-#     total += i
-#     i += 1
-#
-# print(f"""Součet je {total}!""")
+def soucet_n():
+    n = int(input("Zadejte číslo n: "))
+    i = 1
+    total = 0
+
+    while i <= n:
+        total += i
+        i += 1
+
+    print(f"""Součet je {total}!""")
 
 
 # # 3 - tabulka hodnot gon fcí
-# import math as m
-#
-# angle = 0
-# print("uhel\t\tsin \t\t\tcos \t\t\ttg  \t\t\tcotg")
-# while angle <= 90:
-#     angle_rad = m.radians(angle)
-#     value_sin = "%0.3f" % m.sin(angle_rad)
-#     value_cos = "%0.3f" % m.cos(angle_rad)
-#     if angle == 90:
-#         value_tg = "UD   "
-#     else:
-#         value_tg = "%0.3f" % m.tan(angle_rad)
-#
-#     if angle == 0:
-#         value_cotg = "UD   "
-#     else:
-#         value_cotg = "%0.3f" % m.sin(angle_rad)
-#     print(f"""{angle}°\t\t\t{value_sin}\t\t\t{value_cos}\t\t\t{value_tg}\t\t\t{value_cotg}""")
-#     angle += 5
+import math as m
+
+def gon_fce():
+    angle = 0
+    print("uhel\t\tsin \t\t\tcos \t\t\ttg  \t\t\tcotg")
+    while angle <= 90:
+        angle_rad = m.radians(angle)
+        value_sin = "%0.3f" % m.sin(angle_rad)
+        value_cos = "%0.3f" % m.cos(angle_rad)
+        if angle == 90:
+            value_tg = "UD   "
+        else:
+            value_tg = "%0.3f" % m.tan(angle_rad)
+
+        if angle == 0:
+            value_cotg = "UD   "
+        else:
+            value_cotg = "%0.3f" % (1 / m.tan(angle_rad))
+        print(f"""{angle}°\t\t\t{value_sin}\t\t\t{value_cos}\t\t\t{value_tg}\t\t\t{value_cotg}""")
+        angle += 5
+
+
+# # 4 Převod do bin z decimal
+def prevod():
+    number_d = int(input("Zadejte číslo: "))
+    number_b = ''
+    vysledek_deleni = number_d // 2
+    zbytek = number_d % 2
+    number_b += str(zbytek)
+
+    def check_lenth(zbytky):
+        for i in range(3, 999, 5):
+            x = 1
+            if len(zbytky) == i + x:
+                x += 1
+                return True
+
+    while vysledek_deleni:
+        zbytek = vysledek_deleni % 2
+        vysledek_deleni = vysledek_deleni // 2
+        number_b = str(zbytek) + number_b
+        if check_lenth(number_b):
+            number_b = " " + number_b
+
+    print(number_b)
+
+while True:
+    user_input = input("Zadejte úkol(prevod, heslo, soucet_n, gon_fce, pro konec \"end\"): ")
+    if user_input == "prevod":
+        prevod()
+    elif user_input == "heslo":
+        heslo()
+    elif user_input == "soucet_n":
+        soucet_n()
+    elif user_input == "gon_fce":
+        gon_fce()
+    elif user_input == "end":
+        break
+    else: print("Zadej to správně!")
