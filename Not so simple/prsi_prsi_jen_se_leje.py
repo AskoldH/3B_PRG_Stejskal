cf0c21a728d4f44931c0ff02e11ecf8fd844247f
AskoldH <horcicka.asd@gmail.com>
2022-02-25 21:06:50 +0100

    25.2.2022 Prší prší update

--- Not so simple/prsi_prsi_jen_se_leje.py
+++ Not so simple/prsi_prsi_jen_se_leje.py
@@ -6,7 +6,7 @@ class Deck():
         pass
 
     def __init__(self):
-        card_values = ["ESO", "2", "3", "4", "5", "6", "7", "8", "9", "10", "KLUK", "DÁMA", "KRÁL"]
+        card_values = ["ESO", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JUNÁK", "DÁMA", "KRÁL"]
         self.generated_deck = []
         self.top_card = False
 
@@ -52,17 +52,34 @@ class CardPile():
         self.top_card = deck.pop(0)
 
     def put_card_on_top(self, deck, card):
-        deck.append(self.top_card)
-        self.top_card = card
+        # check validity
+        if self.top_card[0] == card[0] or self.top_card[1] == card[1]:
+            # put into deck
+            deck.append(self.top_card)
+            self.top_card = card
+            # check for special cards
+            if card[1] == 7:
+                return 2
+            elif card[0] == '♠' and card[1] == 'K':
+                return 4
+            elif card[1] == 'E':
+                return "pass cycle"
+            return False
+
+        # casted off wrong card
+        input(
+            '\n' + f"No takhle to nepůjde!! {card} rozhodně nemůžeš dát na {self.top_card}! (Pro pokračování zmáčkni ENTER)")
+        return card
 
     def show_top_card(self):
-        print(f"Na vrcholu hromádky je karta {self.top_card}")
+        print("\n" + f"Na vrcholu hromádky je karta {self.top_card}")
 
 
 class HumanPlayer():
     def __init__(self):
 
-        self.name = input("Tvé jméno: ")
+        self.name = input(
+            "Zdravím, vítej u snad všem známe hry prší, tentokrát ale v Pythonu!! Prosím zadej své jméno: ")
         self.card_deck = []
 
     def get_cards(self, deck, quantity=1):
@@ -94,33 +111,96 @@ class PCPlayer():
 
 
 def main():
+    # init
     deck = Deck()
     human = HumanPlayer()
     pc = PCPlayer()
     pile = CardPile(deck.generated_deck)
 
+    # get first 4 cards
     human.get_cards(deck.generated_deck, 4)
     pc.get_cards(deck.generated_deck, 4)
 
     deck.show_len()
 
+    player_move = True
+    pc_move = True
+
     while True:
+        # win human
         if not len(human.card_deck):
             print(f"\nMusím prohlásit, že {human.name} je velice dobrým hráčem prší, gratuluji vyhrál jsi!!")
             break
-        elif len(pc.card_deck):
-            print(f"Ovávám se, že jsi prohrál, počítač má prázdnou ruku ale ty v ní máš ještě {' '.join(str(x) for x in human.card_deck)}!!")
-            break
-        human.show_cards()
-        user_input = input("Zadejte pořadí karty, kterou chcete odhodit nebo \"vzít\" pro kartu z hromádky: ")
 
-        try:
-            pile.put_card_on_top(deck.generated_deck, human.put_card_into_pile(int(user_input) - 1))
-        except:
+        # win pc
+        elif not len(pc.card_deck):
+            print(
+                f"Obávám se, že jsi prohrál, počítač má prázdnou ruku ale ty v ní máš ještě {' '.join(str(x) for x in human.card_deck)}!!")
+            break
+
+        # human player move
+        while player_move:
+            pc_move = True
+            pile.show_top_card()
+            human.show_cards()
+            print(f"Počet karet tvého protivníka je {len(pc.card_deck)}!")
+            user_input = input("Zadejte pořadí karty, kterou chcete odhodit nebo \"vzít\" pro kartu z hromádky: ")
+
+            # user wanna get card
             if user_input == "vzít":
                 human.get_cards(deck.generated_deck, 1)
-            else:
-                print("!!Zadejte to správně!!\n")
+                break
+
+            # check if input is number
+            try:
+                int(user_input)
+            except:
+                print("!!Zadejte správně pořadí karty nebo \"vzít\"(př. \"2\" pro kartu druhou z leva)!!")
+                continue
+
+            # check user input index validity
+            if int(user_input) - 1 > len(human.card_deck) or int(user_input) - 1 < 0:
+                print("!!Zadal jsi špatné pořadí mimo možnosti!!")
+                continue
+
+            # cast off card
+            returned_value = pile.put_card_on_top(deck.generated_deck, human.put_card_into_pile(int(user_input) - 1))
+
+            # player put on top "attack" card
+            if returned_value == 2 or returned_value == 4:
+                pc.get_cards(deck.generated_deck, returned_value)
+
+            # player casted off top ace
+            elif returned_value == "pass cycle":
+                pc_move = False
+
+            # player casted off not valid card
+            elif returned_value:
+                human.card_deck.insert(int(user_input) - 1, returned_value)
+            break
+
+        # pc move
+        while pc_move:
+            player_move = True
+            valid_card_found = False
+            for card in pc.card_deck:
+                if card[0] == pile.top_card[0] or card[1] == pile.top_card[1]:
+                    returned_value = pile.put_card_on_top(deck.generated_deck, card)
+                    pc.card_deck.remove(card)
+                    # pc put on top "attack" card
+                    if returned_value == 2 or returned_value == 4:
+                        human.get_cards(deck.generated_deck, returned_value)
+
+                    # pc casted off top ace
+                    elif returned_value == "pass cycle":
+                        player_move = False
+                    valid_card_found = True
+                    break
+            if not valid_card_found:
+                pc.get_cards(deck.generated_deck)
+            #print(f"Počet karet v decku protivníka: { len(pc.card_deck) }")
+            break
+
 
 
 if __name__ == '__main__':
